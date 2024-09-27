954d8b9942a18efac272f02e4ac41e0672cb2c49
isemae <foxlis97@naver.com>
2024-09-27 17:18:14 +0900

    update Review dto

--- SSAFit_BE/src/main/java/model/dto/Review.java
+++ SSAFit_BE/src/main/java/model/dto/Review.java
@@ -1,65 +1,74 @@
 package model.dto;
 
-public class Review {
+import java.io.Serializable;
+import java.util.Date;
+
+public class Review implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private static int no = 1;
 	private int reviewId;
 	private String userName;
 	private String content;
 	private String regDate;
-	
+
 	public Review() {
-		
 	}
-	
-	public Review(int reviewId, String userName, String content, String regDate) {
-		this.reviewId = reviewId;
+
+	public Review(String userName, String content) { 
+		this.reviewId = no++;
 		this.userName = userName;
 		this.content = content;
-		this.regDate = regDate;
+		this.regDate = new Date().toString();
+	}
+
+	public static int getNo() {
+		return no;
+	}
+
+	public static void setNo(int no) {
+		Review.no = no;
 	}
 
 	public int getReviewId() {
 		return reviewId;
 	}
 
-
 	public void setReviewId(int reviewId) {
 		this.reviewId = reviewId;
 	}
-
 
 	public String getUserName() {
 		return userName;
 	}
 
-
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
-
 
 	public String getContent() {
 		return content;
 	}
 
-
 	public void setContent(String content) {
 		this.content = content;
 	}
-
 
 	public String getRegDate() {
 		return regDate;
 	}
 
-
 	public void setRegDate(String regDate) {
 		this.regDate = regDate;
 	}
 
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 
 	@Override
 	public String toString() {
-		return "Review [reviewId=" + reviewId + ", userName=" + userName + ", Content=" + content + ", regDate="
+		return "Review [reviewId=" + reviewId + ", userName=" + userName + ", content=" + content + ", regDate="
 				+ regDate + "]";
 	}
 
