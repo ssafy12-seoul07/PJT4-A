9862847c06bc3c41bbda5412e089c106d821ea14
inaemin <inaemin@gmail.com>
2024-09-27 17:19:46 +0900

    ReviewRepository commit

--- SSAFit_BE/src/main/java/model/repository/ReviewRepository.java
+++ SSAFit_BE/src/main/java/model/repository/ReviewRepository.java
@@ -1,6 +1,23 @@
 package model.repository;
 
+import java.util.List;
+
+import model.dto.Review;
+
 public interface ReviewRepository {
+	//전체 리뷰 가져오기
+	public abstract List<Review> selectAll();
 	
+	//리뷰 조회하기
+	public abstract Review selectOne(int id);
+
+	//리뷰 등록하기
+	public abstract void insertReview(Review review);
+
+	//리뷰 수정하기
+	public abstract void updateReview(Review review);
+
+	//리뷰 삭제하기
+	public abstract void deleteReview(int id);
 
 }
--- SSAFit_BE/src/main/java/model/repository/ReviewRepositoryImpl.java
+++ SSAFit_BE/src/main/java/model/repository/ReviewRepositoryImpl.java
@@ -1,5 +1,53 @@
 package model.repository;
 
-public class ReviewRepositoryImpl {
+import java.util.*;
 
+import model.dto.Review;
+
+public class ReviewRepositoryImpl implements ReviewRepository {
+	
+	private static ReviewRepository repo = new ReviewRepositoryImpl();
+	
+	private List<Review> list = new ArrayList<>();
+	private Map<Integer, Review> reviews = new HashMap<>();
+	
+	private ReviewRepositoryImpl() {
+		reviews.put(1, new Review("김싸피", "리뷰입니다."));
+	}
+	
+	public static ReviewRepository getInstance() {
+		return repo;
+	}
+
+	@Override
+	public List<Review> selectAll() {
+		List<Review> tmp = new ArrayList<>();
+		for(int key : reviews.keySet()) {
+			tmp.add(reviews.get(key));
+		}
+		
+		return tmp;
+	}
+	
+	@Override
+	public Review selectOne(int id) {
+		return reviews.get(id);
+	}
+
+	@Override
+	public void insertReview(Review review) {
+		reviews.put(review.getReviewId(), review);	//ID를 키로 해서 맵으로관리
+		list.add(review); //리스트로 목록을 관리
+	}
+
+	@Override
+	public void updateReview(Review review) {
+		reviews.put(review.getReviewId(), review); //맵으로
+	}
+
+	@Override
+	public void deleteReview(int id) {
+		reviews.remove(id);
+	}
+	
 }
