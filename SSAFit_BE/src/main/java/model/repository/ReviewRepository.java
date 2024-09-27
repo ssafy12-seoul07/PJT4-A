e9bad76709a5d1e9e0352ec524de3dc60cb31988
inaemin <inaemin@gmail.com>
2024-09-27 16:41:44 +0900

    change package

--- SSAFit_BE/src/main/java/model/repository/ReviewRepository.java
+++ SSAFit_BE/src/main/java/model/repository/ReviewRepository.java
@@ -1,4 +1,4 @@
-package repository;
+package model.repository;
 
 public interface ReviewRepository {
 
--- SSAFit_BE/src/main/java/model/service/ReviewServiceImpl.java
+++ SSAFit_BE/src/main/java/model/service/ReviewServiceImpl.java
@@ -1,4 +1,4 @@
-package service;
+package model.service;
 
 public class ReviewServiceImpl {
 
